81a4783ab80b6e2f116bd35a1aa55e2fe4a5c9dd
Stiv Kapten <stkapt@gmail.com>
2017-08-03 09:25:02 +0300

    last commit before sending

--- src/Dragon.java
+++ src/Dragon.java
@@ -9,6 +9,7 @@ import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 
 
 /**
@@ -19,11 +20,30 @@ public class Dragon {
     public int scaleThickness = 0;
     public int clawSharpness = 0;
     public int wingStrength = 0;
-    public int fireBreath = 0;
+    public int fireBreath = -1;
 
+    public ArrayList<ArrayList> combinations = new ArrayList<>();
+    public ArrayList<ArrayList> permutations = new ArrayList<>();
+
+    public boolean findingDragons = false;
+    public boolean victorious = false;
     ArrayList<Integer> stats = new ArrayList<>();
     String gameId = "";
     String payload = "";
+
+    String message = "";
+
+    /*
+    Set the counter to how many times you wish to fight the knights.
+     */
+    public void run() {
+        int counter = 0;
+        while (counter < 100) {
+            counter++;
+            getGame();
+            checkWeather();
+        }
+    }
 
     public void getGame() {
         try {
@@ -87,12 +107,25 @@ public class Dragon {
                 String output;
                 while ((output = br.readLine()) != null) {
                     sb.append(output);
-                    System.out.println(sb.toString());
+
+                    String jsonInput = sb.toString();
+
+//                    Gson gson = new Gson();
+//                    Dragon weather = gson.fromJson(jsonInput, Dragon.class);;
+//                    if (weather.message != null) {
+//                        System.out.println("weather is " + weather.message);
+//                    } else {
+//                        System.out.println("message element not present or value is null");
+//                    }
+
+//                    System.out.println(sb.toString());
 
                     if (sb.toString().contains("storm")) {
+                        System.out.println("Stormy weather");
                         // do nothing?
                     }
                     else if (sb.toString().contains("rain")) {
+                        System.out.println("Weather is rainy");
                         scaleThickness = 5;
                         clawSharpness = 10;
                         wingStrength = 5;
@@ -102,6 +135,7 @@ public class Dragon {
                         HttpPut();
                     }
                     else if (sb.toString().contains("normal")) {
+                        System.out.println("Weather is normal");
                         scaleThickness = 4;
                         clawSharpness = 2;
                         wingStrength = 4;
@@ -111,7 +145,18 @@ public class Dragon {
                         HttpPut();
                     }
                     else if (sb.toString().contains("fog")) {
+                        System.out.println("Weather is foggy");
+                        //TODO give us some fog, and we'll think of a solution
+                    }
+                    else if (sb.toString().contains("dry")) {
+                        System.out.println("Weather is dry");
+                        scaleThickness = 10;
+                        clawSharpness = 0;
+                        wingStrength = 10;
+                        fireBreath = 0;
 
+                        assignStats();
+                        HttpPut();
                     }
                 }
 
@@ -129,11 +174,14 @@ public class Dragon {
     }
 
     public void findWinningDragons() {
-
+        findingDragons = true;
         int counter = 0;
-        int solutionCount = 0;
+        int permutationCount = 0;
+        int combinationCount = 0;
 
-        while (counter <= 14641) {
+        getGame();
+
+        while (counter <= (Math.pow(11, 4))) {
             fireBreath++;
             counter++;
             String payload = "";
@@ -154,18 +202,23 @@ public class Dragon {
             if (scaleThickness + clawSharpness + wingStrength + fireBreath == 20) {
                 //&& scaleThickness < 9 && clawSharpness < 9 && wingStrength < 9 && fireBreath < 9
 
-                solutionCount++;
+                permutationCount++;
                 assignStats();
                 HttpPut();
 
-//                Collections.sort(stats);
-//                if (!arrayList.contains(stats)) {
-//                    arrayList.add(stats);
-//                    System.out.println(scaleThickness + " " + clawSharpness + " " + wingStrength + " " + fireBreath);
-//                    solutionCount++;
+                if (victorious) {
+                    Collections.sort(stats);
+                    if (!combinations.contains(stats)) {
+                        combinations.add(stats);
+                        System.out.println(scaleThickness + " " + clawSharpness + " " + wingStrength + " " + fireBreath);
+                        combinationCount++;
+                    }
                 }
             }
-        System.out.println(solutionCount);
+            findingDragons = false;
+            System.out.println("There we're " + combinationCount + " different combinations and "
+                    + permutationCount + " different permutations.");
+        }
     }
 
     /**
@@ -206,9 +259,17 @@ public class Dragon {
                 String output;
                 while ((output = br.readLine()) != null) {
                     sb.append(output);
-                    if (!sb.toString().contains("Defeat")) {
+                    if (findingDragons) {
+                        if (!sb.toString().contains("Defeat")) {
+                            victorious = true;
+                            System.out.println(sb.toString());
+                            System.out.println(stats);
+
+                        } else {
+                            victorious = false;
+                        }
+                    } else {
                         System.out.println(sb.toString());
-                        System.out.println(stats);
                     }
                 }
 
@@ -224,12 +285,6 @@ public class Dragon {
     public static void main(String[] args) {
         Dragon insta = new Dragon();
 //        insta.findWinningDragons();
-
-        int counter = 0;
-        while (counter < 1) {
-            counter++;
-            insta.getGame();
-            insta.checkWeather();
-        }
+        insta.run();
     }
 }
